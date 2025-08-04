709dc33ef44810e05a21777e3a011d5cb4aa9301
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-04 23:50:47 +0530

    17486

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import ContactForm
+def Contact_us(request):
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('contact_us')
+    else:
+        form=ContactForm()
+    return render(request,'home/contact_us.html',{'form':form})
 
 # Create your views here.
