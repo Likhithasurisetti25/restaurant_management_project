eb997eb9aa4f8afb8882074f48ffcaa40914ea0a
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-04 23:36:10 +0530

    17483

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from products.models import Menu
+cladd Order(models.Model):
+    STATUS_CHOICES=[
+        ('PENDING','Pending'),
+        ('DELIVERD','Delivered'),
+        ('CANCELLED','Cancelled'),
+    ]
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    items=models.Many_To_ManyField(Menu)
+    total_amount=models.DecimalField(max_digit=8,decimal_places=2)
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='PENDING')
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
 # Create your models here.
