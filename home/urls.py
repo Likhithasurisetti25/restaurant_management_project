893e1978b5b007d65782035698e24ccfc363c8b8
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-06 00:58:52 +0530

    17489

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('restaurant-location/',views.restaurant_location,name='restaurant_location'),
+    path('menu/',views.menu_view,name='menu'),
     
 ]
