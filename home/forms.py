f2f6319656dd4dfea169cb755fad57fdac88f378
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-06 00:12:03 +0530

    17486

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Contact
-class ContactFrom(forms.ModelForm):
+class ContactForm(forms.ModelForm):
     class Meta:
         model=Contact
         fields=['name','email']
