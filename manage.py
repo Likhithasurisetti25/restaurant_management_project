ac52ddac743aab023413d60b112ac2649b0e48f7
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-06 22:51:34 +0530

    17485

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from home.models import Restaurant
+Restaurant.objects.create(name="Likhitha Kitchen")
+exit()
 
 
 def main():
