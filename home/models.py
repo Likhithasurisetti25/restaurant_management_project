f94b94aa82cb5550eb16af2b02621e7a97f64cb6
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-06 22:49:26 +0530

    17485

--- home/models.py
+++ home/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+class Restaurant(models.Model):
+    name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+        
 class MenuItem(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField()
     price=models.DecimalField(max_digits=6,decimal_places=2)
     def __str__(self):
         return self.name
+
 
 # Create your models here.
