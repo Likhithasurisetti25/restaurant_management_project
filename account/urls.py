e387c8c68194dd8a50ccc0ddc2e91e266975ead3
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-21 21:50:35 +0530

    35353

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path(",views.home,name='home'),
+path('menu/',views.menu,name='menu'),
+path('contact/',views.contact,name='contact'),
     
 ]
