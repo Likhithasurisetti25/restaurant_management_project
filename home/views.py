f036f5ea6c9dfe3bbdaad06e881b803c6a3b5259
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-21 23:27:33 +0530

    35356

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 from .forms import ContactForm
 from .models import Restaurant
 def homepage(request):
     restaurant=Restaurant.objects.first()
-    return render(request,'home/home.html',{'restaurant':restaurant if restaurant else "Tasty Bites"})
+    context={"phone_number":settings.RESTAURANT_PHONE}
+    return render(request,'home/home.html',context{'restaurant':restaurant if restaurant else "Tasty Bites"})
 def restaurant_location(request):
     return render(request,'home/restaurant_location.html')
 def Contact_us(request):
