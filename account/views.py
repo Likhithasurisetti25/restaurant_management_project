847a19083099392b9856909ab47ca77de7371f5d
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-21 23:11:07 +0530

    35351

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 def home(request):
     context={'restaurant':"Tasty Bites"}
     return render (request,'home.html',context)
+    return render(request,"home.html",{"restaurant_name":settings.RESTAURANT_NAME})
 
 # Create your views here.
