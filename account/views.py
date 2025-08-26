7a279a1f6413c427cd76332934a2f403f221e24a
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-26 08:07:07 +0530

    38693

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.conf import settings
 def home(request):
     context={'restaurant':"Tasty Bites"}
-    return render (request,'home.html',context)
-    return render(request,"home.html",{"restaurant_name":settings.RESTAURANT_NAME})
+    currentyear=datetime.now().year
+    return render(request,"home.html",{"restaurant_name":settings.RESTAURANT_NAME,"year":currentyear})
 
 # Create your views here.
