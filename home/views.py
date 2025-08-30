814841a5a638990e559a08bae9e155472df3845c
Likhithasurisetti25 <surisettilikhitha@gmail.com>
2025-08-30 21:52:25 +0530

    38696

--- home/views.py
+++ home/views.py
@@ -3,9 +3,14 @@ from django.conf import settings
 from .forms import ContactForm
 from .models import Restaurant
 def homepage(request):
-    restaurant=Restaurant.objects.first()
-    context={"phone_number":settings.RESTAURANT_PHONE}
-    return render(request,'home/home.html',context{'restaurant':restaurant if restaurant else "Tasty Bites"})
+    try:
+        restaurant=Restaurant.objects.first()
+        context={"phone_number":settings.RESTAURANT_PHONE}
+        return render(request,'home/home.html',context{'restaurant':restaurant if restaurant else "Tasty Bites"})
+    except DatabaseError:
+        return render(request,'home/home.html',context)
+    except Exception as e:
+        return HttpResponse(f"Unexpected error:{str(e)}",status=500)
 def restaurant_location(request):
     return render(request,'home/restaurant_location.html')
 def Contact_us(request):
